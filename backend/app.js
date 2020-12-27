d3701503da284066254c0c87c2d52065846ffcf3
Dimitris Kalpiris <dimitrisd2538@gmail.com>
2020-12-27 17:12:24 +0200

    added connection to database

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,24 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
 const app = express()
 const port = 3000
 
+//app.use(cors());
+//app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
 app.get('/products', (req, res) => {
-  const prod  = {name: "bla", price: "44"}
+  const prod  = {name: "bla", price: "45"}
   res.send(prod)
 })
 
