5d5d7fc3c99b16b1af83c2d08a978c27bfc4ca56
Dimitris Kalpiris <dimitrisd2538@gmail.com>
2020-12-28 13:16:48 +0200

    Added routes and created models folder

---
+++ backend/routes/products.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+let Product = require('../models/exercise.model');
+
+router.route('/').get((req, res) => {
+  Product.find()
+    .then(products => res.json(products))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  const id = req.body.id;
+  const price = Number(req.body.duration);
+  const category = req.body.category;
+
+  const newProduct = new Product({
+    id,
+    price,
+    category,
+  });
+
+  newProduct.save()
+  .then(() => res.json('Product added!'))
+  .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  Product.findById(req.params.id)
+    .then(product => res.json(product))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Product.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Product.findById(req.params.id)
+    .then(product => {
+      product.id = req.body.id;
+      product.price = Number(req.body.duration);
+      product.category = req.body.category;
+
+      product.save()
+        .then(() => res.json('Products updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
