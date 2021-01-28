435348a5499f310528237aad8730918d6c086ec7
Dimitris Kalpiris <dimitrisd2538@gmail.com>
2021-01-28 13:19:04 +0200

    Added button

--- ui/src/components/Product.js
+++ ui/src/components/Product.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Product = ({ product }) => {
     return (
-        <div className="container">
+        <div className='container'>
             <p>{product.description}</p>
             <p>{product.price}</p> 
             <p>{product.category}</p>
