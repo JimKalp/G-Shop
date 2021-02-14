6828437673d175480fa85915b3a6caee79c2bd5d
Nikos Koutsovasilis <nikotheract11@gmail.com>
2021-02-14 17:18:26 +0200

    Make navbar responsive

--- ui/src/components/navbar.component.js
+++ ui/src/components/navbar.component.js
@@ -61,8 +61,19 @@ class Navbar extends Component {
         <Link to="/" className="navbar-brand">
           G-Shop
         </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo03"
+          aria-controls="navbarTogglerDemo03"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <div className="container">
-          <div className="collapse navbar-collapse">
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
             <ul className="navbar-nav mr-auto">
               <li className="navbar-item">
                 <Link to="/products" className="nav-link">
